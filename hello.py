5b4a3e663e000715d4b69dd69a2335b4ebf12600
simkhadasujan07@gmail.com <simkhadasujan07@mail.com>
2024-05-28 20:42:51 +0545

    first day

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("hello world")
+age = 20
+print(age)
+age = 30.2
+print(age)
+first_name = "Sujan"
+print(first_name)
+first_name = input("Enter your name: ")
+print("hello " + first_name)
+b_year = input("Enter the birth year:")
+age = 2024 - int(b_year)
+print(age)
+# calculator
+num1 = float(input("enter the first number: "))
+num2 = float(input("input the second number: "))
+add = num1 + num2
+print("sum: " + str(add))
+hello = "this is the messaage"
+print(hello.upper())
+print(hello.find("i"))
+print("is" in hello)
